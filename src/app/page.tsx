e379b8d9da94b50b41bbc4754bde5192d0842dad
Ivan <85517246+IvanTumanow@users.noreply.github.com>
2025-09-28 15:48:12 +0300

    new test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <>
             <h1>Фронт часть сайта VUTEX</h1>
-            <h2>тест</h2>
+            <h2>тест22</h2>
         </>
     );
 }
